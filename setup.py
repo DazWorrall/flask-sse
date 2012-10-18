a83ef0ffd53fa25922a81951bbf20c92e62a038c
Darren Worrall <darren@iweb.co.uk>
2012-10-18 21:52:26 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import sys
+
+setup(name='flask_sse',
+      version='0.1',
+      author='Darren Worrall',
+      author_email='daz@dwuk.net',
+      url='https://git.iweb.co.uk/DazWorrall/flask_sse',
+      description="Flask extension providing SSE support",
+      py_modules=['flask_sse',],
+      provides=['flask_sse',],
+      install_requires=[l.strip() for l in open('requirements.txt').readlines()],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2',
+                  ],
+     )
